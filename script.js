db76145e054c3cbd038e41b9fec7874d24bf55ee
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-14 16:04:16 +0100

    add: array of contacts

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ createApp ({
     data() {
         return {
 
+            // oggetto mio profilo
+            myProfile: {
+
+                name: "Franesco",
+                img: "img/avatar-boolzap/avatar_1.jpg",
+
+            },
+
+            // array of contacts
+            contacts: [
+                {
+                    name: "Paola",
+                    img: "img/avatar-boolzap/avatar_5.jpg",
+                },
+
+                {
+                    name: "Mario",
+                    img: "img/avatar-boolzap/avatar_4.jpg",
+                },
+
+                {
+                    name: "Giacomo",
+                    img: "img/avatar-boolzap/avatar_3.jpg",
+                },
+
+                {
+                    name: "Giorgio",
+                    img: "img/avatar-boolzap/avatar_2.jpg",
+                },
+            ],
+
         }
     },
 
@@ -13,7 +44,7 @@ createApp ({
     },
 
     mounted() {
-        
+
     }
 
-})
+}).mount('#app')
